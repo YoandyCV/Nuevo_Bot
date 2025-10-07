694fc9d15f522da498c483cc8041037e45d359d1
YoandyCV <yoandycalvelo@gmail.com>
2025-10-07 17:35:59 -0400

    zohomail

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Configuración para Zoho Mail
-radr = "tu_email@zohomail.com"  # Tu email de Zoho
+radr = "yoandyc@zohomail.com"  # Tu email de Zoho
 imapserver = "imap.zoho.com"    # Servidor IMAP de Zoho
 smtpserver = "smtp.zoho.com"    # Servidor SMTP de Zoho  
-smtpserverport = 587
+smtpserverport = 465
 pwd = os.getenv('ZOHO_PASSWORD')  # Contraseña de Zoho
 sadr = "yoacv@nauta.cu"  # Email que puede enviar comandos
 check_freq = 10  # Revisar cada 10 segundos
